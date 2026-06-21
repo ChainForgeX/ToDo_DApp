6d04e8005f88e286c6d5fba0689b82d5fe1b70cf
Jeevan Yadav <jeevanyadav9898@gmail.com>
2026-06-21 18:32:51 +0530

    Update ToDo.jsx

--- ToDo.jsx
+++ ToDo.jsx
@@ -8,7 +8,7 @@ function ToDo() {
     const [task, setTask] = useState("");
 
     const contractAddress =
-        "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+        "CONTRACT ADDRESS";
 
     const addTask = async () => {
 
